fb3f6b70773ba599034089ba6b0e6e36df655c59
Yileng Yao <yilengyao@Yilengs-MacBook-Air-2.local>
2024-01-25 23:05:37 +0800

    comment out imports

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-const path = require('path');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
-const { logger } = require('./middleware/logEvents');
-const errorHandler = require('./middleware/errorHandler');
-const verifyJWT = require('./middleware/verifyJWT');
-const cookieParser = require('cookie-parser');
-const credentials = require('./middleware/credentials');
-const mongoose = require('mongoose');
-const connectDB = require('./config/dbConn');
+// const path = require('path');
+// const cors = require('cors');
+// const corsOptions = require('./config/corsOptions');
+// const { logger } = require('./middleware/logEvents');
+// const errorHandler = require('./middleware/errorHandler');
+// const verifyJWT = require('./middleware/verifyJWT');
+// const cookieParser = require('cookie-parser');
+// const credentials = require('./middleware/credentials');
+// const mongoose = require('mongoose');
+// const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 9001;
 
 // Middlewares
