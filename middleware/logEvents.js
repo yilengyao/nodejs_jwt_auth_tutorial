04d69a8e4e634e4019fbb10ca4cf93e981c21145
Yileng Yao <yilengyao@Yilengs-MacBook-Air-2.local>
2024-01-26 14:50:01 +0800

    comment out fs and fspromise in logevents

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -1,9 +1,9 @@
 const { format } = require('date-fns');
 const { v4: uuid } = require('uuid');
 
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-const path = require('path');
+// const fs = require('fs');
+// const fsPromises = require('fs').promises;
+// const path = require('path');
 
 const logEvents = async (message, logName) => {
     // const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
