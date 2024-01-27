e23e326ace9304515a4d12d93bd36e91fd96f274
Yileng Yao <yilengyao@Yilengs-MacBook-Air-2.local>
2024-01-27 09:44:03 +0800

    comment out uuid

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -1,5 +1,5 @@
 const { format } = require('date-fns');
-const { v4: uuid } = require('uuid');
+// const { v4: uuid } = require('uuid');
 
 // const fs = require('fs');
 // const fsPromises = require('fs').promises;
