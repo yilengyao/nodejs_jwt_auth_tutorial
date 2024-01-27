73b000c61d2e75a53c7ee537bfcbdef4f8421905
Yileng Yao <yilengyao@Yilengs-MacBook-Air-2.local>
2024-01-27 10:17:44 +0800

    do not destrucutre uuid on import

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -1,5 +1,6 @@
 const { format } = require('date-fns');
 // const { v4: uuid } = require('uuid');
+const uuid = require('uuid');
 
 const fs = require('fs');
 const fsPromises = require('fs').promises;
