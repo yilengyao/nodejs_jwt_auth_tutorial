93043f8f0ae2363800eb12c5dd50f4d5bbcdd319
Yileng Yao <yilengyao@Yilengs-MacBook-Air-2.local>
2024-01-28 08:29:07 +0800

    add verifyjwt, *route. errorhandler

--- index.js
+++ index.js
@@ -36,5 +36,20 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root'));
 app.use("/home", require("./routes/home"));
 
+app.use(verifyJWT);
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
+app.use(errorHandler);
+
 // connection
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
